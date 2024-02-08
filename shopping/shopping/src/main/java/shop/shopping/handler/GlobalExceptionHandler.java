68904a17d079a089fa8989a3ca5cba9278d25a5a
habin song <habin959@gmail.com>
2024-02-08 17:45:28 +0900

    코드 수정!

---
+++ shopping/shopping/src/main/java/shop/shopping/handler/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package shop.shopping.handler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+
+@Controller
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(value = Exception.class)
+    public String handleException(Exception e,Model model){
+        model.addAttribute("errorMessage",e.getMessage());
+        return "members/errorView";
+    }
+}
