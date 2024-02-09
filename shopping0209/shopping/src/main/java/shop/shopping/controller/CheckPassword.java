35ed4341308a82631f0b24daf01f2f6936dc59d8
kwonyongheon <156667503+kwonyongheon@users.noreply.github.com>
2024-02-09 23:04:17 +0900

    0209작업
    *로그인 및 회원가입 배경화면 그라데이션 추가 + 로그인,회원가입 화면 로고 클릭시 메인페이지로 이동
    *errorview페이지 에러메세지 수정
    *회원수정페이지 이름변경까지 적용

---
+++ shopping0209/shopping/src/main/java/shop/shopping/controller/CheckPassword.java
@@ -0,0 +1,11 @@
+package shop.shopping.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CheckPassword {
+    private String password;
+    private String new_pwd;
+}
---
+++ shopping0209/shopping/src/main/java/shop/shopping/domain_entity/constant/RepImgYn.java
@@ -0,0 +1,5 @@
+package shop.shopping.domain_entity.constant;
+
+public enum RepImgYn {
+    Y, N
+}
